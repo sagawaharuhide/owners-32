45225199c8c65f18634627ea52fc42bc91582ef1
riho.fujino <riho.fujino@gmail.com>
2018-08-09 11:55:09 +0900

    areaが二つになっていたので削除

--- db/migrate/20180809012428_create_plans.rb
+++ db/migrate/20180809012428_create_plans.rb
@@ -3,7 +3,6 @@ class CreatePlans < ActiveRecord::Migration[5.1]
     create_table :plans do |t|
       t.string :producer, null: false
       t.string :area, null: false
-      t.string :area, null: false
       t.date   :dead_line, null: false
       t.timestamps
     end
