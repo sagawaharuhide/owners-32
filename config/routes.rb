6af3b584a090b8e31d548f9c21fe816f7b720c5d
sagawa311 <harusuke311@gmail.com>
2018-08-10 17:36:32 +0900

    set-action-contoroller-view
    ユーザーマイページに必要なルーティング、コントローラー、ビューの追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get 'orders/confirm'
   resources :plans, only: [:index, :show]
   resources :orders, only: [:index, :show]
-  resources :users, only: [:index]
+  resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
