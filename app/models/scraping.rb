1452bbd526acc8d0ac96bdec05e7dc963ccc1b06
riho.fujino <riho.fujino@gmail.com>
2018-08-10 11:45:42 +0900

    インデント調整

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -1,25 +1,25 @@
 require 'mechanize'
 
-class Scarping
+class Scraping
 
   def self.plans_urls
     links = []
-    agent = Mechanaize.new
-    current_page = agent.get("https://owner-style.com/")
+    agent = Mechanize.new
+    current_page = agent.get("https://owner-style.com")
     elements = current_page.search('.plan_card_medium')
     elements.each do |ele|
-      links << ele.get_attribute('href')
+    links << ele.get_attribute('href')
     end
     links.each do |link|
-      get_plans_information("https://owner-style.com/" + link)
+    get_plans_information("https://owner-style.com" + link)
     end
   end
 
   def self.get_plans_information(link)
-    agent = Mechanaize.new
-    page = agant.get(link)
+    agent = Mechanize.new
+    page = agent.get(link)
     producer = page.at('.organization_name').inner_text
-    area = page.at('.location_name').innder_text
+    area = page.at('.location_name').inner_text
     dead_line = page.at('.date').inner_text
     plan = Plan.new(producer: producer, area: area, dead_line: dead_line)
     plan.save
