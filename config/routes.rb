a891d560324c1f3c6c0bac6f5469dc05d371d0b2
keitasuga <keita.sugahara@gmail.com>
2018-08-10 16:09:21 +0900

    Add root_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'plans#index'
   get 'orders/confirm'
   resources :plans, only: [:index, :show]
   resources :orders, only: [:index, :show]
