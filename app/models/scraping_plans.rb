74f741017c5f0b8edc85c50bf274c8db1730eb8c
riho.fujino <riho.fujino@gmail.com>
2018-08-11 15:27:41 +0900

    Planのスクレイピングファイルを修正
    動作しなかったため修正

--- app/models/scraping_plans.rb
+++ app/models/scraping_plans.rb
@@ -1,28 +1,31 @@
 require 'mechanize'
 
-class Scraping
+ class Scraping_plans
 
-  def self.plans_urls
+   def self.plans_urls
     links = []
     agent = Mechanize.new
     current_page = agent.get("https://owner-style.com")
     elements = current_page.search('.plan_card_medium')
     elements.each do |ele|
-    links << ele.get_attribute('href')
+      links << ele.get_attribute('href')
     end
-    links.each do |link|
-    get_plans_information("https://owner-style.com" + link)
-    end
-  end
 
-  def self.get_plans_information(link)
+    links.each do |link|
+      get_plans_information("https://owner-style.com" + link)
+    end
+   end
+
+   def self.get_plans_information(link)
     agent = Mechanize.new
     page = agent.get(link)
     producer = page.at('.organization_name').inner_text
     area = page.at('.location_name').inner_text
     dead_line = page.at('.date').inner_text
-    plan = Plan.where(producer: producer, area: area, dead_line: dead_line).first_or_initialize
+    img_url = page.at('.cover_img_wrap').get_attribute('style').match(/https?:\/\/[\S]+.jpg/).to_s
+    title = page.at('.cover_img_wrap h1').inner_text
+    plan = Plan.where(producer: producer, area: area, dead_line: dead_line, img_url: img_url, title: title).first_or_initialize
     plan.save
-  end
+   end
 
-end
+ end
