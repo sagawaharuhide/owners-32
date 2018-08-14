daba2eb831ac940470c4031c4e2312e8253521d3
sagawa311 <harusuke311@gmail.com>
2018-08-14 16:34:04 +0900

    Edit-update-action
    不要な＠の削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,11 +11,11 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
-    @user.last_name = params[:last_name]
-    @user.first_name = params[:first_name]
-    @user.email = params[:email]
-    @user.save
+    user = User.find(params[:id])
+    user.last_name = params[:last_name]
+    user.first_name = params[:first_name]
+    user.email = params[:email]
+    user.save
     redirect_to user_path(current_user)
   end
 
