ef6c9310d13449e19702a4aa5358a03cf6e05da5
sagawa311 <harusuke311@gmail.com>
2018-08-15 19:36:55 +0900

    edit-routes.rb
    ユーザーのアドレスのルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   resources :plans, only: [:index, :show] do
     resources :orders, only: [:index, :show]
   end
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update] do
+    get 'addresses', on: :collection
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
