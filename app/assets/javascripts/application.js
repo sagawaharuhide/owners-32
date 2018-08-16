94bb5a77957992fdd7eb0e693cf7277c2b63dded
riho.fujino <riho.fujino@gmail.com>
2018-08-16 21:01:42 +0900

    注文フォームに自動計算機能を追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,9 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require turbolinks
+
 //= require_tree .
-//= require jquery3
+//= require jquery
+//= require jquery_ujs
 //= require popper
 //= require bootstrap-sprockets
