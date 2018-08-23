00ae96bd8734fdc797790aeced6eb7b8c32f3bb9
keitasuga <keita.sugahara@gmail.com>
2018-08-23 11:30:03 +0900

    Add  method set_course

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,19 +1,17 @@
 class OrdersController < ApplicationController
+  before_action :set_course
 
   def new
-    @course = Course.find(params[:course_id])
     @plan = @course.plan
     @order = Order.new
   end
 
   def input
-    @course = Course.find(params[:course_id])
     @addresses = current_user.addresses
     @order = Order.new(order_params)
   end
 
   def confirm
-    @course = Course.find(params[:course_id])
     @plan = @course.plan
     @order = Order.new(order_params)
     @address = @order.address
@@ -23,7 +21,6 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @course = Course.find(params[:course_id])
     @addresses = current_user.addresses
     @plan = @course.plan
     @order = Order.new(order_params)
@@ -40,6 +37,10 @@ class OrdersController < ApplicationController
   private
     def order_params
       params.require(:order).permit(:quantity, :address_id).merge(course_id: params[:course_id])
+    end
+
+    def set_course
+      @course = Course.find(params[:course_id])
     end
 
 end
