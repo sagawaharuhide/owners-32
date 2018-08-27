a81a78515dbf70f30494125229b8965f35a1d6fb
riho.fujino <riho.fujino@gmail.com>
2018-08-27 21:42:15 +0900

    Add-sort-function-by-price

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -10,8 +10,15 @@ class PlansController < ApplicationController
     @courses = Course.where(plan_id: params[:id])
   end
 
-  def search
+  def sort_by_category
     @plans = Plan.where(category_id: params[:id])
+    respond_to do |f|
+      f.json { render json: @plans.to_json( include: [:courses]) }
+    end
+  end
+
+  def sort_by_price
+    @plans = Plan.includes(:courses).order('courses.price ASC')
     respond_to do |f|
       f.json { render json: @plans.to_json( include: [:courses]) }
     end
