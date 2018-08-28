6014ab35e2ca86bc47412dcd25850ec08ce7a396
riho.fujino <riho.fujino@gmail.com>
2018-08-28 13:18:25 +0900

    募集締切までの日数表示

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -4,4 +4,15 @@ class Plan < ApplicationRecord
   belongs_to :category
   has_many :likes, dependent: :destroy
   has_many :users, through: :likes
+
+  def display_deadline
+    today = Date.today
+    deadline = dead_line.to_date
+    diff = (deadline - today).to_i
+    if diff.between?(0,30)
+      return diff
+    end
+  end
+
 end
+
